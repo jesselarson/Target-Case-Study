7cdcc8ac7d0c09f1309ca581225b0922d4128a40
Jesse Larson <jesse@jesselarson.com>
2022-12-12 09:50:25 -0600

    Remove non-generic retrieval method

--- App/ProductViewer/ProductViewer/Services/APIClient.swift
+++ App/ProductViewer/ProductViewer/Services/APIClient.swift
@@ -15,36 +15,6 @@ enum NetworkingError: Error {
 }
 
 struct APIClient {
-    func retrieveDeals(completion: @escaping (Result<[Product], Error>) -> Void) {
-        // TODO: Make this a configuration item. Create a new URLRequest and send to method
-        let url = URL(string: "https://api.target.com/mobile_case_study_deals/v1/deals")!
-        let request = URLRequest(url: url)
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if (response as? HTTPURLResponse)?.statusCode != 200 {
-                completion(.failure(NetworkingError.unsuccessfulRequestError))
-            } else {
-                guard let data = data else {
-                    completion(.failure(NetworkingError.badDataError))
-                    return
-                }
-                
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                
-                do {
-                    let productList = try decoder.decode(ProductList.self, from: data)
-                    completion(.success(productList.products))
-                } catch {
-                    completion(.failure(error))
-                }
-            }
-        }
-        task.resume()
-    }
-    
     func retrieveData<T: Decodable>(_ type: T.Type, url: URL?, completion: @escaping (Result<T, Error>) -> Void) {
         guard let url = url else {
             completion(.failure(NetworkingError.badDataError)) // TODO: make this a url error
